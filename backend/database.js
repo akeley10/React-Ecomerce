3c70e1b98627b25c0b32dca391f4ac4131a5afb0
Alejandro Martel <91066025+akeley10@users.noreply.github.com>
2025-08-11 00:44:19 +0100

    Conectando los datos del formulario del login con el backend para ver si recibe los datos correctamente

--- backend/database.js
+++ backend/database.js
@@ -1,3 +1,7 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = 3000;
 const mysql = require('mysql')
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -6,9 +10,25 @@ const connection = mysql.createConnection({
   database: 'react-ecomerce'
 })
 
-connection.connect((err) => {
-    if (err) throw err;
-    console.log("Conectado a la base de datos!");
-  });
 
-connection.end()
+app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(express.json()); 
+
+
+// Ruta de ejemplo
+app.get('/', (req, res) => {
+  res.send('Servidor Express funcionando 🚀');
+});
+
+
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+
+  console.log("Datos recibidos:", email, password);
+
+
+});
+// Arrancar servidor
+app.listen(port, () => {
+  console.log(`Servidor escuchando en http://localhost:${port}`);
+});
--- src/Components/pages/Form.tsx
+++ src/Components/pages/Form.tsx
@@ -1,16 +1,38 @@
+import { useState } from "react";
 const Form = () => {
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+
+  const sendData = (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+    fetch("http://localhost:3000/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body:JSON.stringify( {
+        "email": email,
+        "password": password,
+      })
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+      })
+
+  };
+
   return (
     <>
     <div className="w-full flex justify-center items-center">
-      <form className="card w-100 content-center p-8 bg-base-100 shadow-lg flex flex-col gap-y-4 mt-50">
+      <form onSubmit={sendData} className="card w-100 content-center p-8 bg-base-100 shadow-lg flex flex-col gap-y-4 mt-50">
         <h2 className="text-center text-3xl font-bold mb-5 mt-5">Login</h2>
         <div className="flex flex-col text-center">
         <label className="text-2xs text-left">Email</label>
-        <input className="border border-[#394e6a33] p-2 mb-5 rounded-xl" type="email"></input>
+        <input className="border border-[#394e6a33] p-2 mb-5 rounded-xl" value={email} onChange={(e)=>setEmail(e.target.value)} name="email" type="email"></input>
         <label className="text-2xs text-left">Password</label>
-        <input className="border border-[#394e6a33] p-2 mb-5 rounded-xl" type="text"></input>
-        <button className="bg-[#057aff] p-2 text-white rounded-6xs cursor-pointer" type="submit">Login</button>
-       
+        <input className="border border-[#394e6a33] p-2 mb-5 rounded-xl" value={password} onChange={(e)=>setPassword(e.target.value)} name="password" type="text"></input>
+        <button className="bg-[#057aff] p-2 text-white rounded-6xs cursor-pointer" type="submit">Login</button>  
         </div>
       </form>
       </div>
