97e9c5746727e006c4484ded4455785bbd382582
Alejandro Martel <91066025+akeley10@users.noreply.github.com>
2025-09-22 21:00:56 +0100

    cambiando mas urls

--- src/Components/pages/Products.tsx
+++ src/Components/pages/Products.tsx
@@ -9,7 +9,7 @@ const Products = () => {
   
     const [product, setProduct] = useState<Product[]>([]);
       useEffect(()=>{
-        fetch("http://localhost:3000/send-products", {
+        fetch("https://react-ecomerce.akeley10lol.workers.dev//send-products", {
           method: "POST"
         })
 
